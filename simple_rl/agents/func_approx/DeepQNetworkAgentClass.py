883691e7ae7132e8dafe4b8c68589731508f79a5
Dilip <dilip.arumugam@gmail.com>
2017-03-08 19:02:28 -0500

    Adding DQN (unfinished)

---
+++ simple_rl/agents/func_approx/DeepQNetworkAgentClass.py
@@ -0,0 +1,198 @@
+'''
+DeepQNetworkAgentClass.py
+
+Implementation for a Deep Q Network learner
+
+From:
+    Mnih, Volodymyr et al. Human-level control through deep
+    reinforcement learning. Nature 518 7540 (2015): 529-33.
+
+Based on DQN implementation by Christopher Grimm and Melrose Roderick
+'''
+# Python imports.
+from __future__ import division
+import numpy as np
+import tensorflow as tf
+
+# Local imports.
+from ..AgentClass import Agent
+
+class DeepQNetworkAgent(Agent):
+    '''
+    Agent that uses a deep convolutional neural network as a nonlinear Q-function approximator.
+    '''
+
+    def __init__(self, state_shape, state_dtype, num_actions, name="DQN", gamma=0.99, learning_rate=0.00025, batch_size=32,
+                 replay_memory_start=50000, replay_memory_max=1000000, epsilon_start=1.0, epsilon_end=0.01,
+                 epsilon_steps=1000000, update_freq=4, target_copy_freq=10000, history_size=4, error_clip=1):
+        '''
+        Args:
+            state_shape (list): Expected shape of observations (Numpy arrays)
+            state_dtype (str): The data type associated with values stored in observation arrays
+            num_actions (int): The total number of actions available to the agent
+            gamma (float): Discount factor
+            learning_rate (float): Learning rate
+            replay_memory_start (int): Steps to run in the environment under a purely random policy before learning
+            replay_memory_max (int): The maximum number of transitions to store in replay memory
+            epsilon_start (float): Initial chance of randomness
+            epsilon_end (float): Final annealed value of epsilon
+            epsilon_steps (int): The number of steps desired for annealing epsilon from epsilon_start to epsilon_end
+            update_freq (int): The number of actions to execute before performing a training step on the network
+            target_copy_freq (int): The number of steps to take before updating target network weights to those of the current network
+            history_size (int): The total number of frames that should be stacked together to compose a single state
+            batch_size (int): The total number of samples in a minibatch of training
+            error_clip (float): Clipping term for the TD(0) error when training the network
+        '''
+        Agent.__init__(self, name=name, actions=range(num_actions), gamma=gamma)
+        # Tensorflow configuration
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
+
+        # DQN configuration
+        self.state_shape = state_shape
+        self.state_dtype = state_dtype
+        self.num_actions = num_actions
+        self.gamma = gamma
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.replay_memory_start = replay_memory_start
+        self.replay_memory_max = replay_memory_max
+        self.epsilon = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_steps = epsilon_steps
+        self.epsilon_delta = (self.epsilon - self.epsilon_end) / self.epsilon_steps
+        self.update_freq = update_freq
+        self.target_copy_freq = target_copy_freq
+        self.history_size = history_size
+        self.error_clip = error_clip
+        self.replay_buffer = ReplayMemory(self.state_shape, self.state_dtype, self.replay_memory_max, self.history_size)
+
+        # Setup input placeholders
+        self.init_placeholders()
+        # Setup forward pass pipeline for DQN and target DQN
+        self.init_networks()
+        # Setup loss computation
+        self.compute_loss()
+        # Setup training operation
+        self.optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate, decay=0.95, centered=True, epsilon=0.01)
+        self.train_op = self.optimizer.minimize(self.loss, var_list=self.get_variables_with_scope('main'))
+        # Setup operation for target network updates
+        self.target_update_op = self.swap_variables(source_scope='main', dest_scope='target')
+
+        # Initialize the Tensorflow computation graph and variables
+        self.sess.run(tf.global_variables_initializer())
+
+    def run_update_step(self):
+        states, actions, rewards, next_states, terminals = self.replay_buffer.sample(self.batch_size)
+        one_hot_actions = np.zeros((self.batch_size, self.num_actions), dtype=np.float32)
+        one_hot_actions[range(len(actions)), actions] = 1
+
+        _, loss = self.sess.run([self.train_op, self.loss],
+                                feed_dict={self.state: states, self.action: one_hot_actions,
+                                           self.next_state: next_states, self.reward: rewards,
+                                           self.terminal: terminals})
+
+        return loss
+
+    def act(self, state, reward):
+        # TODOs: Write this method with all DQN logic, write new ReplayMemory, test
+
+    # ---------------------------------
+    # ---- TENSORFLOW ----
+    # ---------------------------------
+
+    def get_variables_with_scope(self, *scopes):
+        return map(lambda x: tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=x+'/'), scopes)
+
+    def swap_variables(self, source_scope, dest_scope):
+        return [tf.assign(dv, sv) for sv, dv in zip(self.get_variables_with_scope(source_scope), self.get_variables_with_scope(dest_scope))]
+
+    def conv2d(self, input, kernel, stride, num_in_filters, num_out_filters, rectifier=lambda x:x):
+        with tf.variable_scope('conv_layer_params'):
+            W = tf.get_variable('W', [kernel, kernel, num_in_filters, num_out_filters],
+                                initializer=tf.contrib.layers.xavier_initializer())
+            B = tf.get_variable('B', [num_out_filters], initializer=tf.constant_initializer(0.0))
+        conv_out = rectifier(tf.nn.conv2d(input, W, [1, stride, stride, 1], 'VALID') + B)
+        return conv_out
+
+    def fully_connected(self, input, num_outputs, rectifier=lambda x:x):
+        with tf.variable_scope('fc_layer_params'):
+            W = tf.get_variable('W', [input.get_shape()[1].value, num_outputs],
+                                initializer=tf.contrib.layers.xavier_initializer())
+            B = tf.get_variable('B', [num_outputs], initializer=tf.constant_initializer(0.0))
+        fc = rectifier(tf.matmul(input, W) + B)
+        return fc
+
+    def init_placeholders(self):
+        self.state = tf.placeholder(self.state_dtype, self.state_shape)
+        self.action = tf.placeholder(tf.float32, [None, self.num_actions])
+        self.next_state = tf.placeholder(self.state_dtype, self.state_shape)
+        self.terminal = tf.placeholder(tf.bool, [None])
+        self.reward = tf.placeholder(tf.float32, [None])
+
+    def init_networks(self):
+        with tf.variable_scope('main'):
+            self.main_network = self.get_dqn(self.state)
+        with tf.variable_scope('target'):
+            self.target_network = self.get_dqn(self.next_state)
+
+    def compute_loss(self):
+        # Clip rewards to be between -1 and 1
+        self.clipped_reward = tf.minimum(tf.maximum(self.reward, tf.constant(1.0)), tf.constant(-1.0))
+        is_terminal = tf.cast(tf.logical_not(self.terminal), dtype=tf.float32)
+        self.maxQ = tf.reduce_max(self.target_network, reduction_indices=1)
+        self.td_zero_target = self.r + is_terminal * self.gamma * self.maxQ
+        self.td_zero_error  = tf.reduce_sum(self.action * self.main_network, reduction_indices=1) - self.td_zero_target
+        self.clipped_error = tf.where(tf.abs(self.td_zero_error) < self.error_clip, 0.5 * tf.square(self.td_zero_error),
+                                      self.error_clip * tf.abs(self.td_zero_error))
+
+        self.loss = tf.reduce_sum(self.clipped_error)
+        self.gradients = tf.gradients(self.loss, self.main_network)
+
+    def get_dqn(self, input_place):
+        input = tf.image.convert_image_dtype(input_place, tf.float32)
+        with tf.variable_scope('C1'):
+            c1 = self.conv2d(input, kernel=8, stride=4, num_in_filters=self.history_size, num_out_filters=32, rectifier=tf.nn.relu)
+        with tf.variable_scope('C2'):
+            c2 = self.conv2d(c1, kernel=4, stride=2, num_in_filters=32, num_out_filters=64, rectifier=tf.nn.relu)
+        with tf.variable_scope('C3'):
+            c3 = self.conv2d(c2, kernel=3, stride=1, num_in_filters=64, num_out_filters=64, rectifier=tf.nn.relu)
+            c3 = tf.reshape(c3, [-1] + reduce(lambda x,y: x * y, c3.get_shape().as_list()))
+        with tf.variable_scope('FC1'):
+            fc1 = self.fully_connected(c3, num_outputs=512, rectifier=tf.nn.relu)
+        with tf.variable_scope('O'):
+            q_values = self.fully_connected(fc1, num_outputs=self.num_actions)
+        return q_values
+
+class ReplayMemory():
+    def __init__(self, obs_shape, obs_dtype, capacity, history):
+        self.index = 0
+        self.full = False
+        self.capacity = capacity
+        self.history = history
+        self.obs_shape = obs_shape
+        self.obs_dtype = obs_dtype
+        self.numpy_transpose_shape = range(1, len(self.obs_shape) + 1) + [0]
+
+        self.obs = np.zeros([self.capacity] + list(self.obs_shape), dtype=self.obs_dtype)
+        self.actions = np.zeros(self.capacity, dtype=np.uint8)
+        self.rewards = np.zeros(self.capacity, dtype=np.float32)
+        self.terms = np.zeros(self.capacity, dtype=np.bool)
+
+    def append(self, s, a, r, t):
+        # Notice that the next state will be stored at (self.index + 1) % self.capacity
+        self.obs[self.index, :] = s
+        self.actions[self.index] = a
+        self.rewards[self.index] = r
+        self.terms[self.index] = t
+
+        if self.index + 1 >= self.capacity:
+            self.full = True
+        self.index = (self.index + 1) % self.capacity
+
+    def sample(self, num_samples):
+        pass
+
+    def size(self):
+        return self.capacity if self.full else self.index
